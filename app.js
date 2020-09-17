a046544fa1bb74caefbd7c7c8ccbd5daba9d720c
rgaddam <rajkamal.gaddam@imaginea.com>
2020-09-17 14:23:57 +0530

    created a job end point

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
+
+const jobs = [
+    {
+        id:1,
+        title:"Software Engineer"
+    },
+    {
+        id:2,
+        title:"Senior Software Engineer"
+    }
+];
+
+app.get('/jobs', (req, res) => {
+    res.json(jobs);
+});
+
 //listen express app at port 3000
 app.listen(3000, ()=>{
     console.log("Application running on the port 3000");
